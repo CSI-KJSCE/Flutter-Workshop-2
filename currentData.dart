443b1fca9ea84f2be7c09b26c7b3af44038fe1e7
rauldivya <66913283+rauldivya@users.noreply.github.com>
2020-07-02 23:38:57 +0530

    Add files via upload

---
+++ currentData.dart
@@ -0,0 +1,12 @@
+class CurrentData {
+  dynamic currentData = {
+    "city": "Mumbai",
+    "current_temp": 32,
+    "max_temperature": 33,
+    "min_temperature": 30,
+    "main": "Mild shower",
+    "id": 300,
+    "sunrise": "05:50 hours",
+    "sunset": "19:05 hours",
+  };
+}
