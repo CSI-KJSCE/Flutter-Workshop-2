a01e375be1e2675f7db5b5e2f0cc5c40c43ad502
manan <mrpandya2017@gmail.com>
2020-07-19 00:06:50 +0530

    ui changes done in the final solution

--- final solution/lib/screens/city_screen.dart
+++ final solution/lib/screens/city_screen.dart
@@ -26,84 +26,83 @@ class _CityPageState extends State<CityPage> {
   String typedData;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          padding: EdgeInsets.only(top: 15),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.fitHeight,
-              image: NetworkImage(
-                  "https://cdn.pixabay.com/photo/2019/09/22/16/20/location-4496459_960_720.png"),
-            ),
+    return Scaffold(
+      backgroundColor: Color(0xff878787),
+      body: Container(
+        padding: EdgeInsets.only(top: 15),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fitHeight,
+            image: NetworkImage(
+                "https://cdn.pixabay.com/photo/2019/09/22/16/20/location-4496459_960_720.png"),
           ),
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: Container(
-                  decoration: backButton,
-                  margin: EdgeInsets.all(20),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    elevation: 0,
-                    disabledElevation: 0,
-                    backgroundColor: Color(0xff878787),
-                    child: Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 32, vertical: 64),
-                child: TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        borderSide: BorderSide.none),
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                      fontFamily: "SourceSans",
-                    ),
-                    hintText: "Enter city name ",
-                    icon: Icon(
-                      Icons.location_city,
-                      color: Colors.white,
-                    ),
-                  ),
-                  onChanged: (value) {
-                    typedData = value;
+        ),
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                decoration: backButton,
+                margin: EdgeInsets.all(20),
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.pop(context);
                   },
-                ),
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.pop(context, typedData);
-                },
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.red[100],
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    'Get Weather',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: "SourceSans",
-                      color: Colors.white,
-                    ),
+                  elevation: 0,
+                  disabledElevation: 0,
+                  backgroundColor: Color(0xff878787),
+                  child: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                    size: 20,
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 32, vertical: 64),
+              child: TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide.none),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontFamily: "SourceSans",
+                  ),
+                  hintText: "Enter city name ",
+                  icon: Icon(
+                    Icons.location_city,
+                    color: Colors.white,
+                  ),
+                ),
+                onChanged: (value) {
+                  typedData = value;
+                },
+              ),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context, typedData);
+              },
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.red[100],
+                    borderRadius: BorderRadius.circular(20)),
+                child: Text(
+                  'Get Weather',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "SourceSans",
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
